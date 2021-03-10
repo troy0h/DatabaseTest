e6d3316bf8c22214b343069b955f73d9d8e5ebbc
troy0h <troy0h@hotmail.co.uk>
2021-03-10 13:43:23 +0000

    Moved SHA256 encryptor, random generator and connection string to seperate file named Functions

---
+++ Functions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DatabaseTest
+{
+    class Functions
+    {
+        static readonly string ConnectionString =
+                        "Data Source=desktop-sa03gi7;" +
+                        "Initial Catalog=TestLogin;" +
+                        "Integrated Security=SSPI;";
+        public static SqlConnection conn = new SqlConnection(ConnectionString);
+
+        public static string Sha256(string randomString)
+        {
+            // Encrypt given string to SHA256
+            var crypt = new SHA256Managed();
+            var hash = new StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomString));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+
+        public static string Random(int length, bool numeric)
+        {
+            // Random numbers of length length
+            // Used for UserID
+            if (numeric == true)
+            {
+                const string numbers = "1234567890";
+                var res = new StringBuilder(length);
+                using (var rng = new RNGCryptoServiceProvider())
+                {
+                    int count = (int)Math.Ceiling(Math.Log(numbers.Length, 2) / 8.0);
+                    Debug.Assert(count <= sizeof(uint));
+                    int offset = BitConverter.IsLittleEndian ? 0 : sizeof(uint) - count;
+                    int max = (int)(Math.Pow(2, count * 8) / numbers.Length) * numbers.Length;
+                    byte[] uintBuffer = new byte[sizeof(uint)];
+                    while (res.Length < length)
+                    {
+                        rng.GetBytes(uintBuffer, offset, count);
+                        uint num = BitConverter.ToUInt32(uintBuffer, 0);
+                        if (num < max)
+                        {
+                            res.Append(numbers[(int)(num % numbers.Length)]);
+                        }
+                    }
+                }
+                return res.ToString();
+            }
+
+            // Random characters of length length 
+            // Used for password salt
+            else if (numeric == false)
+            {
+                const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+                var res = new StringBuilder(length);
+                using (var rng = new RNGCryptoServiceProvider())
+                {
+                    int count = (int)Math.Ceiling(Math.Log(alphabet.Length, 2) / 8.0);
+                    Debug.Assert(count <= sizeof(uint));
+                    int offset = BitConverter.IsLittleEndian ? 0 : sizeof(uint) - count;
+                    int max = (int)(Math.Pow(2, count * 8) / alphabet.Length) * alphabet.Length;
+                    byte[] uintBuffer = new byte[sizeof(uint)];
+                    while (res.Length < length)
+                    {
+                        rng.GetBytes(uintBuffer, offset, count);
+                        uint num = BitConverter.ToUInt32(uintBuffer, 0);
+                        if (num < max)
+                        {
+                            res.Append(alphabet[(int)(num % alphabet.Length)]);
+                        }
+                    }
+                }
+                return res.ToString();
+            }
+            else
+            {
+                // If no bool given, return nothing
+                string res = "";
+                return res;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,99 +1,12 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
-using System.Diagnostics;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading;
 
 namespace DatabaseTest
 {
-    static class Variables
-    {
-        static readonly string ConnectionString =
-                        "Data Source=desktop-sa03gi7;" +
-                        "Initial Catalog=TestLogin;" +
-                        "Integrated Security=SSPI;";
-        public static SqlConnection conn = new SqlConnection(ConnectionString);
-    }
-
     class Program
     {
-
-        static string Sha256(string randomString)
-        {
-            // Encrypt given string to SHA256
-            var crypt = new SHA256Managed();
-            var hash = new StringBuilder();
-            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomString));
-            foreach (byte theByte in crypto)
-            {
-                hash.Append(theByte.ToString("x2"));
-            }
-            return hash.ToString();
-        }
-
-        public static string Random(int length, bool numeric)
-        {
-            // Random numbers of length length
-            // Used for UserID
-            if (numeric == true)
-            {
-                const string numbers = "1234567890";
-                var res = new StringBuilder(length);
-                using (var rng = new RNGCryptoServiceProvider())
-                {
-                    int count = (int)Math.Ceiling(Math.Log(numbers.Length, 2) / 8.0);
-                    Debug.Assert(count <= sizeof(uint));
-                    int offset = BitConverter.IsLittleEndian ? 0 : sizeof(uint) - count;
-                    int max = (int)(Math.Pow(2, count * 8) / numbers.Length) * numbers.Length;
-                    byte[] uintBuffer = new byte[sizeof(uint)];
-                    while (res.Length < length)
-                    {
-                        rng.GetBytes(uintBuffer, offset, count);
-                        uint num = BitConverter.ToUInt32(uintBuffer, 0);
-                        if (num < max)
-                        {
-                            res.Append(numbers[(int)(num % numbers.Length)]);
-                        }
-                    }
-                }
-                return res.ToString();
-            }
-
-            // Random characters of length length 
-            // Used for password salt
-            else if (numeric == false)
-            {
-                const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-                var res = new StringBuilder(length);
-                using (var rng = new RNGCryptoServiceProvider())
-                {
-                    int count = (int)Math.Ceiling(Math.Log(alphabet.Length, 2) / 8.0);
-                    Debug.Assert(count <= sizeof(uint));
-                    int offset = BitConverter.IsLittleEndian ? 0 : sizeof(uint) - count;
-                    int max = (int)(Math.Pow(2, count * 8) / alphabet.Length) * alphabet.Length;
-                    byte[] uintBuffer = new byte[sizeof(uint)];
-                    while (res.Length < length)
-                    {
-                        rng.GetBytes(uintBuffer, offset, count);
-                        uint num = BitConverter.ToUInt32(uintBuffer, 0);
-                        if (num < max)
-                        {
-                            res.Append(alphabet[(int)(num % alphabet.Length)]);
-                        }
-                    }
-                }
-                return res.ToString();
-            }
-            else
-            {
-                // If no bool given, return nothing
-                string res = "";
-                return res;
-            }
-        }
-
         static void Main()
         {
             Console.WriteLine("Hello\n" +
@@ -129,7 +42,7 @@ namespace DatabaseTest
 
             // Username
             bool tooLong = true;
-            Variables.conn.Open();
+            Functions.conn.Open();
             while (tooLong == true)
             {
                 Console.WriteLine("Enter a username:");
@@ -146,7 +59,7 @@ namespace DatabaseTest
                 }
                 else
                 {
-                    SqlCommand testUser = new SqlCommand($"SELECT * FROM Users WHERE Username = @userName;", Variables.conn);
+                    SqlCommand testUser = new SqlCommand($"SELECT * FROM Users WHERE Username = @userName;", Functions.conn);
                     testUser.Parameters.Add("@UserName", SqlDbType.NChar);
                     testUser.Parameters["@UserName"].Value = username;
                     object testUserResult = testUser.ExecuteScalar();
@@ -157,7 +70,7 @@ namespace DatabaseTest
                     else
                     {
                         tooLong = false;
-                        Variables.conn.Close();
+                        Functions.conn.Close();
                         Console.WriteLine("Username accepted");
                     }
                 }
@@ -171,34 +84,34 @@ namespace DatabaseTest
             if (inputString == confirm)
             {
                 // Add salt to password encryption
-                salt = Random(16, false);
-                passHash = Sha256(inputString + salt);
+                salt = Functions.Random(16, false);
+                passHash = Functions.Sha256(inputString + salt);
 
                 // Create User ID
                 bool idUnique = true;
                 while (idUnique == true)
                 {
-                    userID = Random(12, true);
-                    SqlCommand testID = new SqlCommand($"SELECT UserID FROM Users WHERE UserID = @UserID;", Variables.conn);
+                    userID = Functions.Random(12, true);
+                    SqlCommand testID = new SqlCommand($"SELECT UserID FROM Users WHERE UserID = @UserID;", Functions.conn);
                     testID.Parameters.Add(new SqlParameter("@UserID", userID));
-                    Variables.conn.Open();
+                    Functions.conn.Open();
                     if (testID.ExecuteScalar() == null)
                     {
                         idUnique = false;
-                        Variables.conn.Close();
+                        Functions.conn.Close();
                     }
                 }
                 
                 // Make a new SQL command to create the user
-                SqlCommand newUser = new SqlCommand($"INSERT INTO Users VALUES(@UserID, @UserName, @PassHash, @Salt);", Variables.conn);
+                SqlCommand newUser = new SqlCommand($"INSERT INTO Users VALUES(@UserID, @UserName, @PassHash, @Salt);", Functions.conn);
                 newUser.Parameters.Add(new SqlParameter("@UserID", userID));
                 newUser.Parameters.Add(new SqlParameter("@UserName", username));
                 newUser.Parameters.Add(new SqlParameter("@PassHash", passHash));
                 newUser.Parameters.Add(new SqlParameter("@Salt", salt));
                 // Execute the new command
-                Variables.conn.Open();
+                Functions.conn.Open();
                 newUser.ExecuteNonQuery();
-                Variables.conn.Close();
+                Functions.conn.Close();
                 Console.WriteLine($"Your account has been created, {username}");
                 Console.WriteLine("Returning to the main menu...");
                 Thread.Sleep(1000);
@@ -242,10 +155,10 @@ namespace DatabaseTest
             } while (key != ConsoleKey.Enter);
 
             // Create new SQL command to find a user
-            using SqlCommand findUser = new SqlCommand($"SELECT * FROM Users WHERE Username = @UserName;", Variables.conn);
+            using SqlCommand findUser = new SqlCommand($"SELECT * FROM Users WHERE Username = @UserName;", Functions.conn);
             findUser.Parameters.Add(new SqlParameter("@UserName", username));
             // Find the line the username is on, and populate variables
-            Variables.conn.Open();
+            Functions.conn.Open();
             using SqlDataReader reader = findUser.ExecuteReader();
             int count = reader.FieldCount;
             while (reader.Read())
@@ -280,7 +193,7 @@ namespace DatabaseTest
             else
             {
                 // Try the password
-                password = Sha256(password + salt);
+                password = Functions.Sha256(password + salt);
                 if (password != passwordHash)
                 {
                     Console.WriteLine("Password Incorrect\n");
@@ -290,7 +203,7 @@ namespace DatabaseTest
                     Console.WriteLine("Password Accepted\n");
                 }
             }
-            Variables.conn.Close();
+            Functions.conn.Close();
 
             Console.WriteLine($"UserID: {userID}");
             Console.WriteLine($"Username: {username}");
